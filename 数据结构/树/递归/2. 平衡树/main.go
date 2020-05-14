3c14249153951faa827bca4f2dc98f2d19db858e
bravealan <447970241@qq.com>
2020-05-14 23:47:26 +0800

    平衡二叉树

--- 数据结构/树/递归/2. 平衡树/main.go
+++ 数据结构/树/递归/2. 平衡树/main.go
@@ -14,7 +14,7 @@ func isBalanced(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	if math.Abs(float64(height(root.Left)) - float64(height(root.Right))) > 1 {
+	if math.Abs(float64(height(root.Left))-float64(height(root.Right))) > 1 {
 		return false
 	} else {
 		return isBalanced(root.Left) && isBalanced(root.Right)
@@ -34,4 +34,19 @@ func max(x, y int) int {
 	}
 
 	return x
+}
+
+func isBalancedHelper(root *TreeNode) (bool, float64) {
+	if root == nil {
+		return true, -1
+	}
+	leftIsBalanced, leftHeight := isBalancedHelper(root.Left)
+	if !leftIsBalanced {
+		return false, 0
+	}
+	rightIsBalanced, rightHeight := isBalancedHelper(root.Right)
+	if !rightIsBalanced {
+		return false, 0
+	}
+	return math.Abs(leftHeight-rightHeight) < 2, 1 + math.Max(leftHeight, rightHeight)
 }

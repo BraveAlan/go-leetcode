b23c90a20c677982d35b1e7058cdd74289501ae9
e <447970241@qq.com>
2020-05-23 20:17:45 +0800

    513. Find Bottom Left Tree Value (Easy)

---
+++ 数据结构/树/层次遍历/2. 得到左下角的节点/main.go
@@ -0,0 +1,34 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 513. Find Bottom Left Tree Value (Easy)
+func findBottomLeftValue(root *TreeNode) int {
+	queue := []*TreeNode{root}
+	result := root.Val
+	for {
+		size := len(queue)
+		if size == 0 {
+			return result
+		}
+		for i := 0; i < size; i++ {
+			t := queue[0]
+			queue = queue[1:]
+			if i == 0 {
+				result = t.Val
+			}
+			if t.Left != nil {
+				queue = append(queue, t.Left)
+			}
+			if t.Right != nil {
+				queue = append(queue, t.Right)
+			}
+		}
+
+	}
+}

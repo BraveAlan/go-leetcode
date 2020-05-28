3d4b5a28c51b86ba406350648bae3f640a40fd64
e <447970241@qq.com>
2020-05-28 11:21:22 +0800

    503

---
+++ 数据结构/栈和队列/6. 循环数组中比当前元素大的下一个元素/main.go
@@ -0,0 +1,22 @@
+package main
+
+// 503. Next Greater Element II (Medium)
+func nextGreaterElements(nums []int) []int {
+	nextMax := make([]int, len(nums)) // 单调递减栈（从栈底到栈顶）
+	for i, _ := range nextMax {
+		nextMax[i] = -1
+	}
+	stack, length := []int{}, len(nums)
+	for i := 0; i < length*2; i++ {
+		index := i % length
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[index] {
+			nextMax[stack[len(stack)-1]] = nums[index]
+			stack = stack[:len(stack)-1]
+		}
+		// 保留第一遍遍历数组时的入栈结果
+		if i < length {
+			stack = append(stack, index)
+		}
+	}
+	return nextMax
+}

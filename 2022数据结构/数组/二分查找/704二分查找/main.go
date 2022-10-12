4c491cdeb6f37e2626419fdd56abfdaab72c3be9
josephfang <josephfang@tencent.com>
2022-10-12 10:10:33 +0800

    add

---
+++ 2022数据结构/数组/二分查找/704二分查找/main.go
@@ -0,0 +1,22 @@
+package main
+
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，
+// 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			// target在左区间
+			high = mid - 1
+		} else {
+			// target在右区间
+			low = mid + 1
+		}
+	}
+	return -1
+}
